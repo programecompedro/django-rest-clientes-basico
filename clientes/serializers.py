5864a72e35b5cb84f757f8d6f753be5b40b6cfbc
pedro miguel <pdemeu@outlook.com>
2021-09-28 11:53:15 -0300

    clean code , validators.py

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,23 +8,15 @@ class ClienteSerializer(serializers.ModelSerializer):
         fields = '__all__'
     
     """ Validações """
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("O CPF deve ter 11 dígitos")
-        return cpf
-    
-    def validate_nome(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError("Não inclua números neste campo")
-        return nome
-    
-    def validate_rg(self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError("O RG deve ter 9 dígitos")
-        return rg
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': "O CPF deve ter 11 dígitos"})     
 
-    def validate_celular(self, celular):
-        if len(celular) < 11:
-            raise serializers.ValidationError("O celular deve ter 11 dígitos")
-        return celular
-    
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': "Não inclua números neste campo"})
+
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg': "Este campo precisa ter 9 dígitos no mínimo"})
+        
+
+        return data
