0df958815e23eea2b8f2178c4f96bd7c165977c5
pedro miguel <pdemeu@outlook.com>
2021-09-28 12:17:51 -0300

    Importando validações para o CPF

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -10,7 +10,7 @@ class ClienteSerializer(serializers.ModelSerializer):
     """ Validações """
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf': "O CPF deve ter 11 dígitos"})     
+            raise serializers.ValidationError({'cpf': "Número de CPF inválido"})     
 
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome': "Não inclua números neste campo"})
--- clientes/validators.py
+++ clientes/validators.py
@@ -1,8 +1,10 @@
 import re
+from validate_docbr import CPF
 
 def cpf_valido(numero_do_cpf):
-    return len(numero_do_cpf) == 11
-
+    cpf = CPF()
+    return cpf.validate(numero_do_cpf)
+    
 def nome_valido(nome):
     return nome.isalpha()
 
